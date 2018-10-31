16da3d3ccb6f67d83b6ecd41e51236cad0063e72
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-10-31 10:31:11 +0100

    Added buttonComponent to "new game" and "forgot password"

--- QuizApp/screens/NewGameScreen.js
+++ QuizApp/screens/NewGameScreen.js
@@ -10,17 +10,21 @@ import {
 } from "react-native";
 import { WebBrowser } from "expo";
 import { MonoText } from "../components/StyledText";
+import ButtonComponent from "../components/ButtonComponent/ButtonComponent";
+
 export default class NewGameScreen extends React.Component {
   render() {
     return (
-      <ScrollView
+      <View
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        <View style={styles.getStartedContainer}>
-          <Text>hej</Text>
-        </View>
-      </ScrollView>
+        <Text>Nytt spel</Text>
+
+        <ButtonComponent title="Spela själv" />
+        <ButtonComponent title="Spela mot random spelare" />
+        <ButtonComponent title="Spela mot en vän" />
+      </View>
     );
   }
 }
